27439739a513f35c49966ad9e5af2edf9bf59da7
goncalveshigino <goncalveshigino20@gmail.com>
2021-07-20 18:32:30 +0100

    feito

--- models/band.js
+++ models/band.js
@@ -1,14 +1,17 @@
-const { v4: uuidV4 } = require('uuid');
 
+
+const { v4: uuidV4 } = require('uuid');
 
 class Band {
 
-    constructor(name = 'no-name') {
+    constructor( name = 'no-name' ) {
         
-        this.id = uuidV4(); // Identificador unico
+        this.id = uuidV4(); // identificador único
         this.name  = name;
         this.votes = 0;
     }
+
 }
+
 
 module.exports = Band;
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,15 +1,16 @@
 const { io } = require('../index');
+
 const Band = require('../models/band');
 const Bands = require('../models/bands');
 
 //Nova instancia da class Bands
 const bands = new Bands();
 
-bands.addBand(new Band('Quenn',));
-bands.addBand(new Band('Bon Jovi',));
-bands.addBand(new Band('Metalica',));
+bands.addBand( new Band( 'Breaking Benjamin' ) );
+bands.addBand( new Band( 'Bon Jovi' ) );
+bands.addBand( new Band( 'Héroes del Silencio' ) );
+bands.addBand( new Band( 'Metallica' ) );
 
-console.log( bands)
 
 
 
@@ -18,43 +19,45 @@ io.on('connection', client => {
 
     console.log('Cliente conectado');
 
-    client.emit('active-bands', bands.getBands() );
+    client.emit('active-bands', bands.getBands());
+    
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
     });
 
 
-    client.on('mensagem', (payload) => {
-        console.log('Mensagem', payload);
-
-        io.emit( 'mensagem', { admin: 'Nova Mensagem'})
-
+    client.on('mensaje', ( payload ) => {
+        console.log('Mensaje', payload);
+        io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
     });
 
-    /*client.on('emitir-mensagem', (payload) => {
-        // io.emit('Nova mensagem', payload ); //emite a todos  
-        client.broadcast.emit('nova-mensagem', payload); //emite a todos menos quem emitiu
-    })*/
-
+  
     client.on('vote-band', (payload) => {
+
         bands.voteBand(payload.id);
         io.emit('active-bands', bands.getBands());
     });
 
     client.on('add-band', (payload) => {
 
-        const newBand = new Band(payload.name);
-        bans.addBand(newBand);
+        const newBand = new Band( payload.name );
+        bands.addBand(newBand);
         io.emit('active-bands', bands.getBands() );
     });
 
-    client.on('delete-band', (payload) => {
+    client.on('delete-band', ( payload ) => {
         
         bands.deleteBand( payload.id );
         io.emit('active-bands', bands.getBands());
 
     });
+    
+
+    /*client.on('emitir-mensagem', (payload) => {
+        // io.emit('Nova mensagem', payload ); //emite a todos  
+        client.broadcast.emit('nova-mensagem', payload); //emite a todos menos quem emitiu
+    })*/
 
 
 });
